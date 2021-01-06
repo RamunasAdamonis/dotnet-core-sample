08c356f7d6b00048a3b2965e926e0493c2db5d59
Ramunas <ramunas@atlantisgames.lt>
2021-01-06 10:20:35 +0200

    Adding test project

---
+++ dotnet-core-sample.api.unitTests/DatabaseTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using dotnet_core_sample.api.ModelConfigurations;
+using System.Threading.Tasks;
+using Xunit;
+using System.Linq;
+using dotnet_core_sample.api.Models;
+
+namespace dotnet_core_sample.api.unitTests
+{
+    public class DatabaseTests
+    {
+        private readonly DbContextOptions<DatabaseContext> _options;
+        public DatabaseTests()
+        {
+            _options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetAllProducts_WithOrdering_ReturnsProduct()
+        {
+            //Arrange
+            using (var context = new DatabaseContext(_options))
+            {
+                var product = new Product();
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+            }
+
+            //Act and assert
+            using (var context = new DatabaseContext(_options))
+            {
+                var products = await context.Products
+                    .OrderBy(x => x.ProductType)
+                    .ThenByDescending(x => x.Price.Amount)
+                    .ToListAsync();
+
+                Assert.Single(products);
+            }
+        }
+    }
+}
---
+++ dotnet-core-sample.api/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using dotnet_core_sample.api.ModelConfigurations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_core_sample.api.Controllers
+{
+    [ApiController]
+    [Route("product")]
+    public class ProductController : ControllerBase
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public ProductController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("sum")]
+        public async Task<ActionResult<object>> GetSum()
+        {
+            var query = _dbContext.Products.Where(x => x.Name == "Product");
+
+            var summary = await _dbContext.Products
+                .Select(x => new { Sum = query.Sum(x => x.Price.Amount), Count = query.Count() })
+                .ToListAsync();
+            return Ok(summary);
+        }
+
+        [HttpGet("max")]
+        public async Task<ActionResult<object>> GetMax()
+        {
+            var summary = await _dbContext.Products
+                .GroupBy(p => p.ProductType, p => p.Price.Amount)
+                .Select(g => new { ProductType = g.Key, Sum = g.Max(x => x) })
+                .ToListAsync();
+            return Ok(summary);
+        }
+    }
+}
